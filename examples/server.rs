18b126aebd555148f13dc1e0c8b69eca72abbc62
suyanlong <592938410@qq.com>
2017-08-08 21:11:29 +0800

    add examples

--- examples/server.rs
+++ examples/server.rs
@@ -4,25 +4,19 @@ extern crate XnetSocket;
 extern crate log;
 extern crate env_logger;
 
-use ws::listen;
+use XnetSocket::listen;
 
 fn main() {
-	// Setup logging
+
 	env_logger::init().unwrap();
-
-	// Listen on an address and call the closure for each connection
+	
 	if let Err(error) = listen("127.0.0.1:3012".to_string(), |out| {
-		// The handler needs to take ownership of out, so we use move
 		move |msg| {
-			// Handle messages received on this connection
 			info!("Server got message '{}'. ", msg);
-
-			// Use the out channel to send messages back
 			out.send(msg)
 		}
 	})
 	{
-		// Inform the user of failure
 		println!("Failed to create Socket due to {:?}", error);
 	}
 }
